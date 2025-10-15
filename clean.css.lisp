fed0aa48cc0580b212a503583f69f1e203a56b9d
Richard Westhaver <ellis@rwest.io>
2025-10-14 20:20:58 -0400

    bump

--- clean.css.lisp
+++ clean.css.lisp
@@ -14,6 +14,7 @@
  :max-width fit-content)
 (code :color (rgb 79/85 16/17 32/85) :background-color (rgb 16/85 1/5 16/85))
 ("details p" :margin 0)
+("input#search" :max-width fit-content)
 (pre 
  :display block
  :border-radius #((px 3) (px 3) (px 3) (px 3))
