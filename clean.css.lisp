4ad2788516a5786eff5eae612072cbe071ecf88a
Richard Westhaver <ellis@rwest.io>
2025-11-04 17:12:03 -0500

    full convert to css.lisp

--- clean.css.lisp
+++ clean.css.lisp
@@ -6,14 +6,14 @@
 (body :font-family monospace :color (rgb 239/255 50/51 83/85) :background-color (rgb 4/85 1/17 2/51))
 (.content :display grid :grid-auto-flow dense :max-width (ch 80) :font-size (px 16))
 (.status :display flex :flex-flow row-reverse)
-("h1, h2, h3, h4, h5, h6" :color cornsilk)
+(#(h1 h2 h3 h4 h5 h6) :color cornsilk)
 ("b, strong" :color (rgb 79/85 16/17 32/85))
 (pre
  :font-family "BQN"
  :overflow overlay
  :max-width fit-content)
 (code :color (rgb 79/85 16/17 32/85) :background-color (rgb 16/85 1/5 16/85))
-("details p" :margin 0)
+((details p) :margin 0)
 (input :max-width fit-content)
 (pre 
  :display block
@@ -25,22 +25,21 @@
  :max-width fit-content)
 (pre.example :border-left #((px 8) solid (rgb 73/255 18/85 29/85)))
 (img :display block :height auto :margin auto :border-radius (px 5))
-("figure > object" :height auto :margin auto)
+((figure > object) :height auto :margin auto)
 (a :font-weight inherit :line-height inherit :color (rgb 44/85 44/51 66/85))
 ("a:hover"
  :text-decoration blink
  :color (rgb 196/255 19/85 82/255)
  :background-color (rgb 2/15 2/15 2/15))
 (table :max-width fit-content :border-style outset :overflow-x overlay)
-("table th.left, table td.left" :text-align left)
-("table th.right, table td.right" :text-align right)
+(#("table th.left" "table td.left") :text-align left)
+(#"table th.right" "table td.right") :text-align right)
 ("#index" :display block :text-align left :color (rgb 13/15 202/255 217/255) :font-size (px 48) :font-stretch ultra-expanded)
-("div #text-index"
+((div "#text-index")
  :color (rgb 79/85 16/17 32/85)
  :display none
  :text-align left
- ;; FIX 2025-10-02: (defun blur)
- :backdrop-filter blur(6px))
+ :backdrop-filter (blur 6))
 ("#org-div-home-and-up"
  :display inline-flex
  :position sticky
@@ -64,20 +63,20 @@
  :top 0
  :right 0)
 (header :margin 0 :padding 0)
-("header h1" :margin 0 :padding 0)
-("#table-of-contents h2" :color palevioletred :padding 0 :margin 0 :text-align right)
-("#table-of-contents li" :list-style none)
+((header h1) :margin 0 :padding 0)
+(("#table-of-contents" h2) :color palevioletred :padding 0 :margin 0 :text-align right)
+(("#table-of-contents" li) :list-style none)
 ("#table-of-contents #text-table-of-contents"
- :backdrop-filter blur(6px)
+ :backdrop-filter (blur 6)
  :display none
  :position sticky
  :overflow-y overlay
  :height 100vh)
 (.subtitle :text-align center :color red :font-weight bold :font-size (em 1.1))
 (.tag :float right :color (rgb 46/85 112/255 48/85))
-("h3 .tag" :right (px 0) :position absolute :color lightpink)
+((h3 .tag) :right (px 0) :position absolute :color lightpink)
 (.org-center :text-align center)
-(".org-dl dt" :font-weight bold)
+((.org-dl dt) :font-weight bold)
 (.title :text-align center)
 (.org-org-ref-acronym :color (rgb 14/15 118/225 0) :text-decoration underline)
 (.org-org-ref-cite :color (rgb 13/17 71/85 13/17) :text-decoration underline)
@@ -110,5 +109,5 @@
 (.org-org-upcoming-deadline :color (rgb 178/255 2/15 2/15))
 (.org-org-upcoming-distant-deadline :color (rgb 0 0 0) :background-color (rgb 1 1 1))
 (.org-org-warning :color (rgb 1 0 0) :font-weight bold)
-("div #footnotes" :padding-bottom (em 1))
+((div "#footnotes") :padding-bottom (em 1))
 (":target" :color (rgb 16/17 39/85 4/17))
